94bcfdb4babdf8e94f3a5d80bb588792e7db4379
4mjeo <4mj.eo@dsm.hs.kr>
2023-03-28 16:55:41 +0900

    programming

---
+++ pro.c
@@ -0,0 +1,50 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b,c;
+//	scanf("%d %d %d",&a,&b,&c);
+//	a = a+(b+c)/60;
+//	b = (b+c%60);
+//	
+//	if(a>23)
+//	printf("%d %d",a%24,b);
+//	else 
+//	printf("%d %d",a,b);
+//	
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b,c;
+//	scanf("%d %d %d",&a,&b,&c);
+//	
+//	b = b+a*60+c;
+//	a = (b/60)%24;
+//	b = b%60;
+//	printf("%d %d",a,b);
+//	
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int h,m;
+//	scanf("%d %d",&h,&m);
+//	m = m-45
+//	h = (m<0) ? h-1:h;
+//	m = (m>0)
+//	if(h>23)
+//	printf("%d %d",h%24,m);
+//	else
+//	printf("%d %d",h,m);
+//	
+//	return 0;
+//}
+
+
+
